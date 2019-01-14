504aecf55789e9b96fbbf0090926b3e44f306524
Chelsea Lee <chelsea.d.lee12@gmail.com>
2019-01-14 09:07:50 -0800

    configure header and navigation

--- docroot/themes/custom/crosscut_festival/src/custom/global.js
+++ docroot/themes/custom/crosscut_festival/src/custom/global.js
@@ -10,23 +10,83 @@
     Drupal.behaviors.bootstrap_barrio_subtheme = {
         attach: function(context, settings) {
 
-            var video = $('#heroVideo');
-            $(video).on('ended', function() {
-                console.log('ended');
-                $('.hero-overlay.video').removeClass('active');
-                $('.hero-overlay-image').animate({
-                  opacity: 1
-                }, 600);
+          // Navigation
+          // Determine if event is enter or tab keypress
+          function a11yClick(e) {
+            if (e.type === 'click') {
+              return true;
+            } else if (e.type === 'keypress') {
+              var code = e.charCode || e.keyCode;
+              if ( code === 32 || code === 13 ) {
+                return true;
+              }
+            }
+          }
+
+          var $dropdownMenu = $('.nav-dropdown');
+          var $toggleButton = $('#toggle');
+          $toggleButton.on('click keypress', function(e) {
+            if (a11yClick(e)) {
+              if ($dropdownMenu.hasClass('active')) {
+                $dropdownMenu.removeClass('active');
+                $(this).attr('aria-expanded', 'false').removeClass('active');
+              }
+              else {
+                $dropdownMenu.addClass('active');
+                $(this).attr('aria-expanded', 'true').addClass('active');
+              }
+            }
+          });
+
+          $dropdownMenu.on('focusout', function() {
+            if ($dropdownMenu.hasClass('active') && e.target !== $dropdownMenu) {
+              $dropdownMenu.removeClass('active');
+              $toggleButton.attr('aria-pressed', 'true').removeClass('active');
+            }
+          });
+
+          // Close menu if document is clicked
+          $(document).on('click', function(e) {
+              if ($dropdownMenu.hasClass('active') && e.target !== $dropdownMenu) {
+                  $dropdownMenu.removeClass('active');
+                  $toggleButton.attr('aria-pressed', 'true').removeClass('active');
+              }
+          });
+
+
+          // Smooth Scroll
+          $('a[href^="#"]').on('click', function(e) {
+            e.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 1000, function() {
+              // add hash to URL
+              window.location.hash = hash;
             });
 
-            $(window).scroll(function() {
-                if ($(this).scrollTop() > 50) {
-                    $('body').addClass("scrolled");
-                } else {
-                    $('body').removeClass("scrolled");
-                }
-            });
+            $('a.cf-nav-link[href^="#"]').removeClass('active');
+            $(this).addClass('active');
+          });
 
+          // Video
+          var video = $('#heroVideo');
+          $(video).on('ended', function() {
+              $('.hero-overlay.video').removeClass('active');
+              $('.hero-overlay-image').animate({
+                opacity: 1
+              }, 600);
+          });
+
+          $(window).scroll(function() {
+              if ($(this).scrollTop() > 50) {
+                  $('body').addClass("scrolled");
+              } else {
+                  $('body').removeClass("scrolled");
+              }
+          });
         }
     };
 
