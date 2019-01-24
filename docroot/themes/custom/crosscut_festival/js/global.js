c7c78f362ab0471df6189b2aa35be8cab2c67a82
Chelsea Lee <chelsea.d.lee12@gmail.com>
2019-01-24 13:57:13 -0800

    remove excess space in news article

--- docroot/themes/custom/crosscut_festival/js/global.js
+++ docroot/themes/custom/crosscut_festival/js/global.js
@@ -103,7 +103,7 @@
             var image_path = domain + data['image'];
             var date = data['created'].slice(0, -8); // remove time from long format date
 
-            var html = '<div class="row no-gutters"><div class="col-sm-6 col-md-3"><div class="img-container"><img class="newsImage" alt="Crosscut Festival News Article" src="' + image_path + '"/></div></div><div class="col-sm-6 col-md-9 article-teaser"><h4>' + data['title'] + '</h4><p>' + data['excerpt'] + '</p><span class="byline">by ' + data['author'] + ' / ' + date + '</span></div></div>';
+            var html = '<div class="row no-gutters"><div class="col-sm-6 col-md-3"><div class="img-container"><img class="newsImage" alt="Crosscut Festival News Article" src="' + image_path + '"/></div></div><div class="col-sm-6 col-md-9 article-teaser"><h4>' + data['title'] + '</h4>' + data['excerpt'] + '<span class="byline">by ' + data['author'] + ' / ' + date + '</span></div></div>';
             $article.append(html);
           }
 
